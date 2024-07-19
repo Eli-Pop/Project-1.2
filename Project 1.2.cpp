26a44adea2bc82b226356a7049f14781d7588a8b
Elias V. <58016431+Eli-Pop@users.noreply.github.com>
2024-07-18 21:12:48 -0500

    Add files via upload

---
+++ Project 1.2.cpp
@@ -0,0 +1,35 @@
+/*
+* Project 1.2
+* This program displays a conversion table of feet to meters.
+* Call this program FtoMTable.cpp.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double f; // holds the length in feet
+	double m; // holds the conversion to meters
+	int counter;
+
+	counter = 0; // line counter is initially set to zero.
+
+	for (f = 1.0; f <= 100.0; f++)
+	{
+		m = f / 3.28; // convert to meters
+		cout << f << " feet is " << m << " meters.\n";
+
+		counter++; // Increment the line counter with each loop iteration.
+
+		// every 10th line, print a blank line
+		if (counter == 10)
+		{
+			cout << "\n"; // output a blank line
+			counter = 0; // reset the line counter
+		}
+	}
+
+	system("pause");
+	return 0;
+}
